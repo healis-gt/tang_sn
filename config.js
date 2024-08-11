6f8e7ff756cbc00e3461631942144c729241de54
healis-gt(Nguyen Trung Duc) <ducnguyen10112003@gmail.com>
2024-08-12 00:25:24 +0700

    Initial commit

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    introTitle: 'Em bé ơi! 🎉',
+    introDesc: `Hôm nay là sinh nhật 20 tuổi của em bé rùi đó 🎂
+    Lần thứ 2 anh được  đón sinh nhật với em bé. 🥳
+    Chúc em có một ngày sinh nhật thật vui vẻ, hạnh phúc 😊
+    Mãi mãi luôn yêu anh ❤️
+    Đằng sau chữ yêu đây là thương 💖
+    Anh yêu em bé nhiều lắm 😘`,    
+    btnIntro: 'Yêu em bé 😘',
+    title: 'Chắc đây là tiếc mục em đã mong chờ lắm rồi phải không 😙',
+    desc: 'Nhưng mà em bé phải tha lỗi cho anh thì mới có được nha 😢 ',
+    btnYes: 'Em tha lỗi cho anh 💖', 
+    btnNo: 'Em không tha lỗi cho anh 😢',
+    question:'Em bé tha lỗi thì có thể nói EM YÊU ANH được không 😙',
+    btnReply: 'Gửi cho anh 💖',
+    reply: 'EM YÊU ANH NHIỀU LẮM LUÔN 💖',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Tối thứ 2 như hẹn anh qua đón nhé công chúa bé nhỏ 😘😘.',
+    btnAccept: 'Dạ yêu anh 💖',
+    //messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
